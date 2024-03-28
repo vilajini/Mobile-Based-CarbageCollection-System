c424fa25281b54f4fbec5a46668daf39b9e9559b
vilajini <vilajini03@gmail.com>
2024-03-28 18:11:47 +0530

    database change

---
+++ CarbageCollection/src/pages/dashboard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <View>
+      <Text>dashboard</Text>
+    </View>
+  )
+}
+
+export default dashboard
+
+const styles = StyleSheet.create({})
